c2bdcd367e0a6f5292e0470999ba4535d733ab93
Chandarahou <78402418+Chandarahou@users.noreply.github.com>
2024-02-28 10:02:27 +0700

    delete

--- android/app/src/main/kotlin/com/example/khmergpt/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.khmergpt
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/services/assests_manager.dart
+++
@@ -1,8 +0,0 @@
-class AssetsManager{
-  static String imagePath = "assets/images";
-  static String userImage = "$imagePath/person.png";
-  static String openailogo = "$imagePath/openai_logo.jpg";
-  static String chatlogo = "$imagePath/chat_logo.png";
-  static String sourLogo = "$imagePath/sourLogo.png";
-  static String sourIcon = "$imagePath/sourIcon.png";
-}
